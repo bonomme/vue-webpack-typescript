5a9f86250337ff94766ac21a5383414611f29545
Brice Guegan <brice.guegan@intercloud.com>
2017-05-29 18:33:56 +0200

    cleaned routes

--- template/src/main.ts
+++ template/src/main.ts
@@ -1,5 +1,4 @@
 import * as Vue from 'vue';
-import { HomeComponent } from './components/home';
 import { router } from './conf/routes';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
