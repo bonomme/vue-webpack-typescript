7c64adc742a58c5d1d52f556305563ef703eea8d
Brice Guegan <brice.guegan@intercloud.com>
2017-05-29 18:29:58 +0200

    updated the base project to only have a nav bar and homepage, added the base.model abstact class

--- template/src/main.ts
+++ template/src/main.ts
@@ -1,21 +1,7 @@
 import * as Vue from 'vue';
-import VueRouter from 'vue-router';
-
 import { HomeComponent } from './components/home';
-import { AboutComponent } from './components/about';
-import { ListComponent } from './components/list';
-import { NavbarComponent } from './components/navbar';
-
-// register the plugin
-Vue.use(VueRouter);
-
-let router = new VueRouter({
-  routes: [
-    { path: '/', component: HomeComponent },
-    { path: '/about', component: AboutComponent },
-    { path: '/list', component: ListComponent },
-  ]
-});
+import { router } from './conf/routes';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 new Vue({
   el: '#app-main',
